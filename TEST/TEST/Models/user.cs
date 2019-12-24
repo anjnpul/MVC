20c487a10b3ceeadd49728bf025abb8cf22a2e8e
ANJAN <anjan.paul3@gmail.com>
2019-12-24 12:21:14 +0530

    1st

---
+++ TEST/TEST/Models/user.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TEST.Models
+{
+    public class user
+    {
+        public int CID { get; set; }
+        [Required(ErrorMessage = "Please Provide Username", AllowEmptyStrings = false)]
+        [DisplayName("User Name")]
+        public string uid { get; set; }
+        [Required(ErrorMessage = "Please Provide Password", AllowEmptyStrings = false)]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string pass { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+    }
+}
